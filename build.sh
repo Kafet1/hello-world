48c3ddd984bcf73ed20a456d244ceaaa804a3789
Kafet1 <hmk.kafet@gmail.com>
2022-06-09 20:49:03 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ mvn install clean package
 # copy war file
 cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war .
 cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war /opt/tomcat/webapps
+cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war /home/kafet
 
 # build Docker images
 docker build -t kafet/tomcat:v1 .
