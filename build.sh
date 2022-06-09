dd5292310c9c61f158baac3cba9ed2543f6ca13d
Kafet1 <hmk.kafet@gmail.com>
2022-06-09 14:10:04 +0700

    update Snake Version

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ mvn install clean package
 
 # copy war file
 cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war .
+cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war /opt/tomcat/webapps
+
 
 # build Docker images
 docker build -t kafet/tomcat:v1 .
