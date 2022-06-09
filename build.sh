33576b4119ee13f30e5cc794bbaf34901a650b50
Kafet1 <hmk.kafet@gmail.com>
2022-06-09 14:42:52 +0700

    delete file

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ mvn install clean package
 cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war .
 cp /var/lib/jenkins/workspace/ci-maven3/webapp/target/webapp.war /opt/tomcat/webapps
 
-
 # build Docker images
 docker build -t kafet/tomcat:v1 .
 
